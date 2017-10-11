9d2abeea90106cb35016e98f4c3ef095ca113ee2
Baltemos <baltemos@gmail.com>
2017-10-11 19:18:39 +0200

    Asm files
    Here's the asm file i wrote

---
+++ bootPrintHex.asm
@@ -0,0 +1,35 @@
+print_hex:
+    pusha
+
+    mov cx, 0
+add 0x40
+
+hexLoop:
+    cmp cx, 4 ; loop 4 times
+    je end
+    
+    mov ax, dx 
+    and ax, 0x000f 
+    add al, 0x30
+    cmp al, 0x39
+    jle step2
+    add al, 7
+
+step2:
+    mov bx, HEX_OUT + 5
+    sub bx, cx
+    mov [bx], al
+    ror dx, 4
+
+    add cx, 1
+    jmp hexLoop
+
+end:
+    mov bx, hexOut
+    call print
+
+    popa
+    ret
+
+hexOut:
+    db '0x0000', 0
---
+++ switch32.asm
@@ -0,0 +1,22 @@
+[bits 16]
+switch32:
+    cli
+    lgdt [gdt_descriptor]
+    mov eax, cr0
+    or eax, 0x1 ; 3. set 32-bit mode bit in cr0
+    mov cr0, eax
+    jmp CODE_SEG:init32
+
+[bits 32]
+init32:
+    mov ax, DATA_SEG
+    mov ds, ax
+    mov ss, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+
+    mov ebp, 0x90000
+    mov esp, ebp
+
+    call start32
